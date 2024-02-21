84a4287d2cc837e5692c7c044372afddd53b07f1
BimmerHex <github@vintagegamestudio.com>
2024-02-21 08:50:56 +0300

    Student/Create View ve StudentController oluşturuldu.
    StudentController oluşturuldu.
    Student/Create View oluşturuldu.
    _Layout.cshtml içerisine Öğrenci Ekle nav-link eklendi.
    _ViewImports.cshtml içerisine Data klasörü using olarak eklendi.

--- obj/Debug/net8.0/efcoreApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/efcoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+632b74f814144ea67c1f092f11b46b28c4264688")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c8a9817c3c49a4378be337df4dc7d91d55bf056")]
 [assembly: System.Reflection.AssemblyProductAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
